19ec028c469b6d520c674cae9dbd0d70ec97ba84
Milan Menezes <milanmenezes@gmail.com>
2018-08-24 15:32:57 +0530

    Update and rename led1.py to led.py

--- led.py
+++ led.py
@@ -9,12 +9,8 @@ GPIO.setup(11, GPIO.OUT)
 
 l=[1,0,0]
 while True:
-	print l
 	GPIO.output(3,l[0])
-	time.sleep(0.3)
 	GPIO.output(5,l[1])
-	time.sleep(0.3)
 	GPIO.output(11,l[2])
 	time.sleep(0.3)
 	l=[l[-1]]+l[:-1]
-#	time.sleep(0.6)
